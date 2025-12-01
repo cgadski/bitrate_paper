b9f822efc20b933affb70730e46ddee253cb9a12
Christopher Gadzinski <me@cgad.ski>
2025-12-01 11:06:39 +0100

    k_sweep_runner

--- project/k_sweep_runner.py
+++ project/k_sweep_runner.py
@@ -1,4 +1,4 @@
-from project.pursuit import Options, go
+from project.k_sweep import Options, go
 from loguru import logger
 from project.misc import grid
 
@@ -6,8 +6,7 @@ if __name__ == "__main__":
     loops = 0
     while True:
         for args in grid(
-            n=[2**10, 2**14, 2**16, 2**20],
-            max_steps=[1],
+            n=[2**8, 2**12, 2**16, 2**20],
         ):
             logger.info(f"Completed loops: {loops}")
             go(Options(k_step=1, d_step=64, **args, device="cuda"))
